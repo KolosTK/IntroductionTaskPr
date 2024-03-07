ecab8a7fd97729c37902dde4e9e33e73b76a90a4
KolosTK <94901694+KolosTK@users.noreply.github.com>
2024-03-07 13:10:59 +0200

    Add index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+import exress from 'express';
+
+const port = 5000;
+
+const app = express();
+const DB_URL ="mongodb+srv://tanyakolosenko1705:<password>@cluster0.jkxjxfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
