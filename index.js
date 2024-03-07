3c49d0a4ac8ed0e6fa2e7ab3302aa3a0531407a5
KolosTK <94901694+KolosTK@users.noreply.github.com>
2024-03-07 15:27:04 +0200

    Change port to 5005

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-import exress from 'express';
+import express from 'express';
+import mongoose from 'mongoose';
+import Student from'/GitHub/IntroductionTaskPr/Models/Student.js';
+// import Homework from'./Models/Homework.js';
 
-const port = 5000;
+const PORT = 5005;
 
 const app = express();
-const DB_URL ="mongodb+srv://tanyakolosenko1705:<password>@cluster0.jkxjxfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+app.use(express.json());
+
+const DB_URL ="mongodb+srv://tanyakolosenko1705:admin@cluster0.jkxjxfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+app.post('/',async(req,res)=>{
+    const {name,age,studentClass}= req.body    
+    const student = await Student.create({name,age,studentClass})
+    res.status(200).json(student)
+})
+
+
+async function startApp()
+{
+    try{
+        await mongoose.connect(DB_URL);
+        app.listen(PORT, ()=>console.log('APP PORT IS '+PORT));
+    }
+    catch(e){
+        console.log(e);
+    }
+
+}
+startApp();
