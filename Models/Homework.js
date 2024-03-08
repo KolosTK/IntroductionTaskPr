9e65715f3fcb5cd3dc31be1484ce76ba0afd7ff7
KolosTK <94901694+KolosTK@users.noreply.github.com>
2024-03-08 10:12:36 +0200

    Fix errors

--- Models/Homework.js
+++ Models/Homework.js
@@ -1,9 +1,9 @@
-import mongoos from 'mongoose';
+import mongoose from 'mongoose';
 
 const Homework =new mongoose.Schema({
-    "title":{type : String, required:true},
-    "description":{type : String, required:true},
-    "dueDate":{type : String, required:true},
-    "studentID":{type : String, required:true}
+    title:{type : String, required:true},
+    description:{type : String, required:true},
+    dueDate:{type : String, required:true},
+    studentID:{type : String, required:true}
 });
 export default mongoose.model('Homework',Homework);
