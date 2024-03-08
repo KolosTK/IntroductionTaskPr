2911e59c427f6ddb4c6bb021779e5c20d89485d8
KolosTK <94901694+KolosTK@users.noreply.github.com>
2024-03-08 10:12:57 +0200

    Change index.js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Student from'/GitHub/IntroductionTaskPr/Models/Student.js';
-// import Homework from'./Models/Homework.js';
+import Student from'./Models/Student.js';
+import Homework from'./Models/Homework.js';
 
 const PORT = 5005;      
 
@@ -9,25 +9,33 @@ const app = express();
 
 app.use(express.json());
 
+<<<<<<< Updated upstream
 const DB_URL ="mongodb+srv://tanyakolosenko1705:irXfH6xT8DqszQSg@cluster0.nhqth5q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+=======
+// const DB_URL ="mongodb+srv://tanyakolosenko1705:admin@cluster0.jkxjxfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+>>>>>>> Stashed changes
 
-app.post('/',async(req,res)=>{
-    const {name,age,studentClass}= req.body    
-    const student = await Student.create({name,age,studentClass})
-    res.status(200).json(student)
+app.get('/',(req,res)=>{
+    res.status(200).json('Hello');
 })
 
+// app.post('/',(req,res)=>{
+//     res.send('This is the post request');
+// })
 
-async function startApp()
-{
-    try{
-        await mongoose.connect(DB_URL);
-        app.listen(PORT, ()=>console.log('APP PORT IS '+PORT));
-    }
-    catch(e){
-        console.log(e);
-    }
+app.listen (PORT, ()=>console.log("server port "+PORT))
 
-}
-startApp();
+
+// async function startApp()
+// {
+//     try{
+//         await mongoose.connect(DB_URL);
+//         app.listen(PORT, ()=>console.log('APP PORT IS '+PORT));
+//     }
+//     catch(e){
+//         console.log(e);
+//     }
+
+// }
+// startApp();
